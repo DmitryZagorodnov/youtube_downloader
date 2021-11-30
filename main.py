6698ed8b1261d3686e63c7d00c98cdb9eda24994
DmitryZagorodnov <43351927+DmitryZagorodnov@users.noreply.github.com>
2021-12-01 00:23:28 +0300

    feat #2: add video downloading in 720 or less resolution

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from pytube import YouTube
 
+url = input("Введите ссылку на видео: \n")
+yt = YouTube(url)
+
+stream = yt.streams.filter(progressive=True).order_by('resolution').desc().first()
+print("Начинаю скачивание...")
+stream.download()
+print(f'Готово! Скачал {stream.title} в качестве {stream.resolution}')
